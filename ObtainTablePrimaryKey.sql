30d35ada772c4dde1163334626ff8be9ddad4391
Greg <80930275+g-crawford@users.noreply.github.com>
2023-01-31 07:10:57 -0800

    Add files via upload
    Obtain the primary key for any table in any database 
    (For use with tables with ONE primary key)

---
+++ ObtainTablePrimaryKey.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [Get].TablePrimaryKey (
+	 @TargetTableDatabase VARCHAR(MAX)
+	,@TargetTableSchema VARCHAR(MAX)
+	,@TargetTableName VARCHAR(MAX)
+	,@PrimaryKey VARCHAR(MAX) OUTPUT
+)
+
+AS
+
+DECLARE @QualifiedTableName AS VARCHAR(MAX) = (
+	@TargetTableDatabase + '.' + @TargetTableSchema + '.[' + @TargetTableName + ']'
+)
+
+DECLARE @Query AS NVARCHAR(1000) = (
+	'SELECT'
+	+ ' @PK = c.name'
+	+ ' FROM ' + @TargetTableDatabase + '.sys.columns AS c'
+	+ ' JOIN ' + @TargetTableDatabase + '.sys.indexes AS i'
+	+ ' ON i.object_id = c.object_id'
+	+ ' AND i.index_id = c.column_id'
+	+ ' WHERE 1=1'
+	+ ' AND c.object_id = OBJECT_ID(''' + @QualifiedTableName + ''')'
+	+ ' AND i.is_primary_key = 1'
+)
+
+EXECUTE sp_executesql 
+@Query
+,N'@PK NVARCHAR(1000) OUTPUT'
+,@PK = @PrimaryKey OUTPUT
